4501100ed0f2c8c93f66837100fe230e71f488f1
Nilton Bento Júnior <nilton.bento@iatec.com>
2022-08-17 09:51:34 -0300

    fix VelocityTracker

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\nilton.bento\fvm\versions\2.8.1"
-export "FLUTTER_APPLICATION_PATH=C:\Users\nilton.bento\Documents\Projs_Flutter_geral\MPFlutterChart\OptimizedGestureDetector\example"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\nilton.bento\Documents\Projs_Gerais\OptimizedGestureDetector\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
